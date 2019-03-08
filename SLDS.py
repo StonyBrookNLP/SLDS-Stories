904e8d3c0a27cde846926390538346f14e237b0c
nwweber <nwweber@cs.stonybrook.edu>
2019-03-08 16:36:51 -0500

    Moved some computation out of the forward func

--- SLDS.py
+++ SLDS.py
@@ -53,72 +53,33 @@ class SLDS(nn.Module):
                                                 nn.Linear(self.hidden_size, self.num_states)
                                               )
 
-
-
     def forward(self, input, seq_lens):
         """
         Args
-            input (Tensor, [batch, num_sents, seq_len]) : Tensor of input ids for the embeddings lookup
-            seq_lens (Tensor [batch, num_sents]) : Store the sequence lengths for each batch for packing
+            input (Tensor, [num_sents, batch, seq_len]) : Tensor of input ids for the embeddings lookup
+            seq_lens (Tensor [num_sents, batch]) : Store the sequence lengths for each batch for packing
         Returns
             output logits (Tensor, [batch, seq_len, vocab size]) : logits for the output words
             means
             state_logits
         """
-        batch_size = input.size(0)
-        num_sents = input.size(1)
+        batch_size = input.size(1)
+        num_sents = input.size(0)
 
-        ehidden = self.encoder.initHidden(batch_size)
-
-        #Encode the entire sequence
-        #Output gives each state output, ehidden is the final state
-        #output (Tensor, [batch, seq_len, hidden*directions] 
-        #ehidden [layers*directions, batch, hidden_size]
-        
-        #Encode representations for each sentence
-        encoded_sents = []
-        for i in range(num_sents):
-            enc_output, ehidden = self.encoder(input[:,i,:], ehidden, seq_lens[i])
-            last_output = gather_last(enc_output, seq_lens[i]) #[batch, encoder_dim]
-            encoded_sents.append(last_output)
-
-        encoded_sents = torch.stack(encoded_sents, dim=0) #[num_sents, batch, encoder dim)
-
-        #Make predictions for each state given the encoded text and sample a state at each time step
-        S_samples = []
-        for i in range(num_sents):
-            context_vect = utils.get_context_vector(encoded_sents, i, future=False)
-            target_vect = encoded_sents[i, :, :]
-            logits = self.state_posterior_factor(torch.cat([target_vect, context_vect], dim=1)) #[batch, num classes]
-            #If Supervised, we could return the logits from here
-            sample = utils.gumbel_softmax_sample(logits, temp=1.0)
-            S_samples.append(sample)
-
-        S_samples = torch.stack(S_samples, dim=0) #[num_sents, batch, num classes]
-
-        #Now Sample the Z's from the approximate posterior
-        Z_samples = []
-        prev_z = Variable(torch.zeros(batch_size, self.hidden_size))
-        for i in range(num_sents):
-            context_vect = utils.get_context_vector(encoded_sents, i, future=True)
-            target_vect = encoded_sents[i, :, :]
-            switch_state = S_samples[i]
-            mean, logvar = dynamics_posterior_factor(prev_z, switch_state, torch.cat([target_vect, context_vect], dim=1))
-            z_sample = utils.normal_sample(mean, logvar)
-            Z_samples.append(z_sample)
-            prev_z = z_sample
-
-        Z_samples = torch.stack(S_samples, dim=0) #[num_sents, batch, num classes]
-
+        encoded_sents = self.encode_sentences(input, seq_lens) #[num_sents, batch, encoder dim)
+        S_samples, state_logits = sample_switch_posterior(self, encoded_sents)
+        Z_samples, Z_means = sample_hidden_posterior(encoded_sents, S_samples) #[num_sents, batch, hidden_dim]
+       
         #Now Evaluate the Liklihood for each sentence
 
         data_logits = []
         for i in range(num_sents):
-            logits = self.data_liklihood_factor(input[:,i,:], Z_samples[i]) #logits is [batch, seq, num_classes]
+            logits = self.data_liklihood_factor(input[i,:,:], Z_samples[i]) #logits is [batch, seq, num_classes]
             data_logits.append(logits)
 
         data_logits = torch.stack(data_logits, dim=0) #[num_sents, batch, seq, num classes]
-        return data_logits, means, state_logits
+
+        return data_logits, Z_means, state_logits
 
     #P(Z_i | Z_i-1, S_i)
     def dynamics_factor(self, prev_z, switch_state):
@@ -157,12 +118,7 @@ class SLDS(nn.Module):
             mean - Tensor [batch, hidden_size]
             logvar - Tensor [batch, hidden_size] (diagonal variance)
         """
-        all_mean_weights = torch.stack([x.weight for x in self.dynamics_mean]) #[num_states X hidden X hidden]
-        #crate single matrix as a convex comb of all dynamics matrix, using switch state as weights, 1 matrix for each batch
-        mean_param = torch.einsum('ijk,bi->bjk', [all_mean_weights, switch_state])  #[batch X hidden X hidden]
-        var_param = torch.einsum('ij,bi->bj', [self.dynamics_logvar, switch_state]) #[batch X hidden]
-
-        means_prior = torch.einsum('bij,bj->bi',[mean_param, prev_z]) #batch X hidden]
+        means_prior, var_param = self.dynamics_factor(prev_z, switch_state):
 
         network_input = torch.cat((prev_z, encoded_data), dim=1) 
         mean_residual = self.dynamics_posterior_network(network_input)
@@ -213,5 +169,88 @@ class SLDS(nn.Module):
  
         logits = torch.stack(dec_outputs, dim=1) #DIMENSION BE [batch x seq x num_classes]
         return logits
+
+    def encode_sentences(self, input, seq_lens):
+        """
+        Encode each sentence into vector representation
+        Args
+            input (Tensor, [num_sents, batch seq_len]) : Tensor of input ids for the embeddings lookup
+            seq_lens (Tensor [num_sents, batch]) : Store the sequence lengths for each batch for packing
+        Returns
+            encoded sentences (Tensor, [num_sents, batch, vocab size]) : logits for the output words
+        """
+
+        num_sents = input.size(0)
+        batch_size = input.size(1)
+
+        encoded_sents = []
+        ehidden = self.encoder.initHidden(batch_size)
+
+        for i in range(num_sents):
+            enc_output, ehidden = self.encoder(input[i,:,:], ehidden, seq_lens[i])
+            last_output = gather_last(enc_output, seq_lens[i]) #[batch, encoder_dim]
+            encoded_sents.append(last_output)
+
+        encoded_sents = torch.stack(encoded_sents, dim=0) #[num_sents, batch, encoder dim)
+        return encoded_sents
+
+
+    def sample_switch_posterior(self, encoded_sents, gumbel_temp=1.0):
+        """
+        Sample the switching state posterior given the encoded data
+        Args
+            encoded_sents (Tensor, [num_sents, batch, encoded dim])  
+        Returns
+            state samples (Tensor, [num_sents, batch, num classes]) : samples from gumbel softmax for each step
+            state logits (Tensor, [num_sents, batch, num classes]) : logits for state prediction, can be used for supervision
+        """
+
+        num_sents = encoded_sents.size(0)
+        S_samples = []
+        logit_list = []
+        for i in range(num_sents):
+            context_vect = utils.get_context_vector(encoded_sents, i, future=False)
+            target_vect = encoded_sents[i, :, :]
+            logits = self.state_posterior_factor(torch.cat([target_vect, context_vect], dim=1)) #[batch, num classes]
+            sample = utils.gumbel_softmax_sample(logits, temp=gumbel_temp)
+            S_samples.append(sample)
+            logit_list.append(logits)
+
+        S_samples = torch.stack(S_samples, dim=0) #[num_sents, batch, num classes]
+        logit_list = torch.stack(logit_list, dim=0)
+        return S_samples, logit_list
+
+
+    def sample_hidden_posterior(self, encoded_sents, S_samples):
+        """
+        Sample the hidden state Z posterior given the encoded data
+        Args
+            encoded_sents (Tensor, [num_sents, batch, encoded dim])  
+            S_samples (Tensor, [num_sents, batch, num_classes]) : the switching state posterior samples
+        Returns
+            hidden samples (Tensor, [num_sents, batch, hidden dim]) : samples from postieror dyncamics distribtion
+            mean (Tensor, [num_sents, batch, hidden dim]) : mean of posterior (for KL calc)
+        """
+        batch_size = input.size(1)
+        num_sents = input.size(0)
+        Z_samples = []
+        means = []
+
+        prev_z = Variable(torch.zeros(batch_size, self.hidden_size))
+        for i in range(num_sents):
+            context_vect = utils.get_context_vector(encoded_sents, i, future=True)
+            target_vect = encoded_sents[i, :, :]
+            switch_state = S_samples[i]
+            mean, logvar = dynamics_posterior_factor(prev_z, switch_state, torch.cat([target_vect, context_vect], dim=1))
+            z_sample = utils.normal_sample(mean, logvar)
+            Z_samples.append(z_sample)
+            means.append(mean)
+            prev_z = z_sample
+
+        Z_samples = torch.stack(Z_samples, dim=0) #[num_sents, batch, hidden]
+        means= torch.stack(means, dim=0) #[num_sents, batch, hidden]
+        return Z_samples, means
+
+
 
 
